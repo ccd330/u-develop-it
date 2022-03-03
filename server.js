74c25e4a13b7f9290cad965a7e5a8d62d9bd3073
claudia.davis <claudiacdavis@gmail.com>
2022-03-03 13:16:43 -0500

    create parties table

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const inputCheck = require('./utils/inputCheck');
-const mysql = require('mysql2');
 const express = require('express');
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -79,13 +79,19 @@ app.delete('/api/candidate/:id', (req, res) => {
 
 // Create a candidate
 app.post('/api/candidate', ({ body }, res) => {
-  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
   if (errors) {
     res.status(400).json({ error: errors });
     return;
   }
+
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-  VALUES (?,?,?)`;
+    VALUES (?,?,?)`;
   const params = [body.first_name, body.last_name, body.industry_connected];
 
   db.query(sql, params, (err, result) => {
@@ -99,7 +105,6 @@ app.post('/api/candidate', ({ body }, res) => {
     });
   });
 });
-
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
